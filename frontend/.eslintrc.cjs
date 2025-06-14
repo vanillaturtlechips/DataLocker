4bd159116a729458130f08cb19f518a9ae61b164
logan <2220110150@office.kopo.ac.kr>
2025-06-14 14:24:14 +0900

    fix: Frontend 설정 파일 완성

--- frontend/.eslintrc.cjs
+++ frontend/.eslintrc.cjs
@@ -41,8 +41,6 @@ module.exports = {
       varsIgnorePattern: '^_' 
     }],
     '@typescript-eslint/no-explicit-any': 'warn',
-    '@typescript-eslint/explicit-function-return-type': 'off',
-    '@typescript-eslint/explicit-module-boundary-types': 'off',
     
     // 일반 규칙
     'no-console': 'warn',
@@ -53,9 +51,6 @@ module.exports = {
     // 코드 스타일
     'quotes': ['error', 'single', { avoidEscape: true }],
     'semi': ['error', 'always'],
-    'comma-dangle': ['error', 'always-multiline'],
-    'object-curly-spacing': ['error', 'always'],
-    'array-bracket-spacing': ['error', 'never'],
     
     // Hook 관련 규칙
     'react-hooks/rules-of-hooks': 'error',
