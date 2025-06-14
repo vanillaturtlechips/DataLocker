acfd5e2264950986fb1a131c1f65d9faed895055
logan <2220110150@office.kopo.ac.kr>
2025-06-14 14:38:09 +0900

    fix: ESLint 설정 완료

--- frontend/.eslintrc.cjs
+++ frontend/.eslintrc.cjs
@@ -1,12 +1,12 @@
 module.exports = {
   root: true,
-  env: { 
-    browser: true, 
-    es2020: true 
+  env: {
+    browser: true,
+    es2020: true
   },
   extends: [
     'eslint:recommended',
-    '@typescript-eslint/recommended',
+    'plugin:@typescript-eslint/recommended',
     'plugin:react/recommended',
     'plugin:react-hooks/recommended',
     'plugin:react/jsx-runtime'
@@ -27,32 +27,23 @@ module.exports = {
     '@typescript-eslint'
   ],
   rules: {
-    // React 관련 규칙
     'react/react-in-jsx-scope': 'off',
     'react/prop-types': 'off',
     'react-refresh/only-export-components': [
       'warn',
       { allowConstantExport: true },
     ],
-    
-    // TypeScript 관련 규칙
-    '@typescript-eslint/no-unused-vars': ['error', { 
+    '@typescript-eslint/no-unused-vars': ['error', {
       argsIgnorePattern: '^_',
-      varsIgnorePattern: '^_' 
+      varsIgnorePattern: '^_'
     }],
     '@typescript-eslint/no-explicit-any': 'warn',
-    
-    // 일반 규칙
     'no-console': 'warn',
     'no-debugger': 'error',
     'prefer-const': 'error',
     'no-var': 'error',
-    
-    // 코드 스타일
     'quotes': ['error', 'single', { avoidEscape: true }],
     'semi': ['error', 'always'],
-    
-    // Hook 관련 규칙
     'react-hooks/rules-of-hooks': 'error',
     'react-hooks/exhaustive-deps': 'warn'
   },
